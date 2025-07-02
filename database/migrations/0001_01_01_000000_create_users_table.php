d23c6acb126803923a176d6bc7449de83543adfb
Impulse <ephraimimpulse17@gmail.com>
2025-07-02 20:16:41 +0100

    refactor:ajout des champs et ajout des relations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,13 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('full_name');
             $table->string('email')->unique();
+            $table->string('username')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('display_name')->nullable();
             $table->string('avatar')->nullable();
-            $table->enum('subscription_tiers', ['free', 'premium', 'team'])->default('free');
+
             $table->timestamp('last_active_at')->nullable();
 
             $table->rememberToken();
