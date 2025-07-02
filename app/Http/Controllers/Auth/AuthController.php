8e16fc5d5cffb649ae6d30209bf3d64a77ecda60
Impulse <ephraimimpulse17@gmail.com>
2025-07-02 20:44:59 +0100

    feat:création de compte utilisateur

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $validated =Validator::make($request->all(),[
+            'full_name'=>'required|string',
+            'email'=>'required|email',
+            'password'=>'required|confirmed|min:6'
+        ]);
+        if($validated->fails()){
+            return response()->json(["errors"=>$validated->errors()]);
+        }
+        $user=User::where('email',$request->email)->first();
+        if($user){
+            return response()->json(["errors"=>"User already exists"]);
+        }
+        try {
+
+            $user =User::create([
+                'full_name'=>$request->full_name,
+                'email'=>$request->email,
+                'password'=>Hash::make($request->password)
+
+            ]);
+            $token=$user->createToken('token')->plainTextToken;
+            $user['token']=$token;
+            return response()->json(["data"=>$user,"message"=>"User created successfully"]);
+
+        }catch (\Exception $e){
+            return response()->json(["errors"=>$e->getMessage()]);
+
+        }
+    }
+}
